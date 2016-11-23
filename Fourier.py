aba16a89cc55a6e09e1c4e8c6e82c05ec79a72f4
Hata <WoodyAllenZombieJesus@yandex.ru>
2016-11-23 20:53:17 +0300

    Скр-скр-скр

--- Fourier.py
+++ Fourier.py
@@ -29,7 +29,7 @@ def h(x):
 		return 1
 
 def g(x):
-	return np.cos(x)
+	return np.cos(3*x)
 
 def mult(a,b):
 	return a*b
@@ -38,31 +38,31 @@ def fourier(f=f,x0=0.0,x1=1.0,n=100,steps=20):
 	mid=(x1-x0)/2.0
 	xs=np.linspace(x0,x1,n,endpoint=True)
 	ys=map(f,xs)
-	a=np.empty([steps+1])
 	b=np.empty([steps+1])
+	a=np.empty([steps+1])
 	b[0]=0.0
 	a[0]=1.0/mid*simps(ys,x=xs)/2.0
 	tmp=np.empty([n])
 	for i in xrange(1,steps+1):
 		for j in xrange(0,n):
-			tmp[j]=np.sin(xs[j]*i)*ys[j]
+			tmp[j]=np.sin(xs[j]*i*np.math.pi/mid)*ys[j]
 		b[i]=1.0/mid*simps(tmp,x=xs)
 		for j in xrange(0,n):
-			tmp[j]=np.cos(xs[j]*i)*ys[j]
+			tmp[j]=np.cos(xs[j]*i*np.math.pi/mid)*ys[j]
 		a[i]=1.0/mid*simps(tmp,x=xs)
 	solution=np.empty([steps+1,n])
 	solution.fill(a[0])
 	for i in xrange(1,steps+1):
 		for j in xrange(0,n):
 			for k in xrange(i,steps+1):
-				solution[k,j]+=a[i]*np.sin(xs[j]*i)+b[i]*np.cos(xs[j]*i)
+				solution[k,j]+=a[i]*np.cos(xs[j]*i*np.math.pi/mid)+b[i]*np.sin(xs[j]*i*np.math.pi/mid)
 	def _graph_animate(t):
 		line.set_ydata(solution[t])
 		return line
 	fig, ax = plt.subplots()
 	ax.plot(xs, ys, c='black')
 	line, = ax.plot(xs, solution[0])
-	anm = ani.FuncAnimation(fig, _graph_animate, frames=steps+1, interval=100, repeat=False)
+	anm = ani.FuncAnimation(fig, _graph_animate, frames=steps+1, interval=500, repeat=False)
 	plt.show()
 
-fourier(f=g,x0=-4.5,x1=4.5,n=101)
+fourier(f=h,x0=-4.5,x1=4.5,n=100,steps=30)
